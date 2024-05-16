7b591bba994690036ec81ab30a89ac9c6d8d7f7f
michalinapepel <misia.pepel@onet.pl>
2024-05-16 14:15:02 +0200

    Add font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { useFonts, Neucha_400Regular } from "@expo-google-fonts/neucha";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "react-native";
 
 export default function TabOneScreen() {
+  let [fontsLoaded, fontError] = useFonts({
+    Neucha_400Regular,
+  });
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.separator} />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +23,17 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    fontFamily: "Neucha_400Regular",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
